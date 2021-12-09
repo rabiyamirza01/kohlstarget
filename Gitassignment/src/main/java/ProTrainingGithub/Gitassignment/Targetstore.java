db61ea2547882f44aeee74eeb769c6d45696ec6b
Rabiya's Windows PC <Rabiya's Windows PC@LAPTOP-9FLITBN3.myfiosgateway.com>
2021-12-08 22:11:36 -0500

    for github

---
+++ Gitassignment/src/main/java/ProTrainingGithub/Gitassignment/Targetstore.java
@@ -0,0 +1,16 @@
+package ProTrainingGithub.Gitassignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Targetstore {
+public static WebDriver driver;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rabiya's Windows PC\\eclipse-workspace\\Gitassignment\\src\\type2\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.navigate().to("https://www.target.com/");
+
+	}
+
+}
